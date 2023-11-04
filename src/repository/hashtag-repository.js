32ef3f1ccbe5763c78ac5043ad7f83126323398f
Ganivada Mouli <vishnumouli0@gmail.com>
2023-11-04 10:27:49 +0530

    Added tweet service with hashtags

--- src/repository/hashtag-repository.js
+++ src/repository/hashtag-repository.js
@@ -55,7 +55,7 @@ class HashtagRepository {
     try {
       const response = await Hashtag.find({
         title: titleList,
-      }).select("title -_id");
+      });
       return response;
     } catch (error) {
       console.log("Error Occurred in Repository Layer");
--- src/services/tweet-service.js
+++ src/services/tweet-service.js
@@ -10,20 +10,19 @@ class TweetService {
     try {
       const content = data.content;
       let tags = content.match(/#[a-zA-Z0-9_]+/g);
-      // .map((tag) => tag.substring(1));
       tags = tags.map((tag) => tag.substring(1));
       const tweet = await this.tweetRepository.create(data);
       let alreadyPresentTags = await this.hashtagRepository.findByName(tags);
-      // .map((tag) => tag.title);
-      alreadyPresentTags = alreadyPresentTags.map((tag) => tag.title);
-      let newTags = tags.filter((tag) => !alreadyPresentTags.includes(tag));
-      // .map((tag) => {
-      //   return { title: tag, tweets: [tweet.id] };
-      // });
+      let titleOfPresentTags = alreadyPresentTags.map((tag) => tag.title);
+      let newTags = tags.filter((tag) => !titleOfPresentTags.includes(tag));
       newTags = newTags.map((tag) => {
         return { title: tag, tweets: [tweet.id] };
       });
-      const response = await this.hashtagRepository.bulkCreate(newTags);
+      await this.hashtagRepository.bulkCreate(newTags);
+      alreadyPresentTags.forEach((tag) => {
+        tag.tweets.push(tweet.id);
+        tag.save();
+      });
       return tweet;
     } catch (error) {
       console.log("Error Occurred in Service Layer");
